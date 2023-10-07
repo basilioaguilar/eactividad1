bd43e78439fd8ad519f9c5106a5534a24e694ed4
basilioaguilar <147267990+basilioaguilar@users.noreply.github.com>
2023-10-07 18:50:18 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const equiposRouter = require('./routes/equipos');
 const patrocinantesRouter = require('./routes/patrocinantes');
 const categoriasRouter = require('./routes/categorias');
